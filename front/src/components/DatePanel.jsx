dde0fb5935f63e466b59ad05e0ea857de64b0ba3
danielcaveman <daniel_caverna@yahoo.com.br>
2021-03-06 09:33:21 -0300

    removed datepicker library

--- front/src/components/DatePanel.jsx
+++ front/src/components/DatePanel.jsx
@@ -1,6 +1,4 @@
-import { useReducer } from "react";
 import styled from "styled-components";
-import { DateRangeInput } from "@datepicker-react/styled";
 
 const Panel = styled.div`
   display: flex;
@@ -11,40 +9,8 @@ const Panel = styled.div`
   font-size: 1.6rem;
 `;
 
-const initialState = {
-  startDate: null,
-  endDate: null,
-  focusedInput: null,
-};
-
-function reducer(state, action) {
-  switch (action.type) {
-    case "focusChange":
-      return { ...state, focusedInput: action.payload };
-    case "dateChange":
-      return action.payload;
-    default:
-      throw new Error();
-  }
-}
-
 function DatePanel() {
-  const [state, dispatch] = useReducer(reducer, initialState);
-  return (
-    <Panel>
-      <DateRangeInput
-        onDatesChange={(data) =>
-          dispatch({ type: "dateChange", payload: data })
-        }
-        onFocusChange={(focusedInput) =>
-          dispatch({ type: "focusChange", payload: focusedInput })
-        }
-        startDate={state.startDate} // Date or null
-        endDate={state.endDate} // Date or null
-        focusedInput={state.focusedInput} // START_DATE, END_DATE or null
-      />
-    </Panel>
-  );
+  return <Panel></Panel>;
 }
 
 export default DatePanel;
