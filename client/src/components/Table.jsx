716b58daa5564ed74bbf001939f5bb7f56883d93
danielcaveman <daniel_caverna@yahoo.com.br>
2021-03-06 19:08:51 -0300

    table simplified

--- client/src/components/Table.jsx
+++ client/src/components/Table.jsx
@@ -1,98 +1,38 @@
 import React from "react";
-import { useState } from "react";
 import MaterialTable from "material-table";
-import { DateService } from "../services/DateService";
-import TimeInput from "./TimeInput";
 
-function Table({ data, updateById, deleteById }) {
-  const dateService = new DateService();
-  const [month] = useState(dateService.generateMonth());
-
-  const deleteRow = (id) => {
-    id && deleteById(id);
-  };
-
-  const getTableData = () => {
-    return month.map((m, index) => {
-      data.forEach((d) => {
-        const isEqual = m.day === d.day;
-        if (isEqual) {
-          m = d;
-        }
-      });
-      return m;
-    });
-  };
-
-  const editComponent = (props) => (
-    <TimeInput
-      type="text"
-      value={props.value}
-      onChange={(e) => props.onChange(e.target.value)}
-    />
-  );
-
+function Table({ data, updateById, deleteById, columns, title }) {
   return (
-    <>
-      <MaterialTable
-        options={{
-          search: false,
-          actionsColumnIndex: -1,
-          rowStyle: (x) => {
-            if (x.tableData.id % 2) {
-              return { backgroundColor: "#fafafa" };
-            }
-          },
-        }}
-        columns={[
-          {
-            title: "Day",
-            field: "day",
-            render: (rowData) =>
-              dateService.formatDate(rowData.day, "MM/DD/YYYY"),
-            editable: "never",
-          },
-          {
-            title: "Begin",
-            field: "begin",
-            editComponent: editComponent,
-          },
-          {
-            title: "Lunch Begin",
-            field: "lunchBegin",
-            editComponent: editComponent,
-          },
-          {
-            title: "Lunch End",
-            field: "lunchEnd",
-            editComponent: editComponent,
-          },
-          {
-            title: "End",
-            field: "end",
-            editComponent: editComponent,
-          },
-        ]}
-        data={getTableData()}
-        title="Month and Year"
-        editable={{
-          onRowUpdate: (rowData) =>
-            new Promise((resolve, reject) => {
-              setTimeout(() => {
-                updateById(rowData);
-                resolve();
-              }, 1000);
-            }),
-          onRowDelete: (rowData) =>
-            new Promise((resolve, reject) => {
-              setTimeout(() => {
-                deleteRow(rowData.id);
-                resolve();
-              }, 1000);
-            }),
-        }}
-      />
-    </>
+    <MaterialTable
+      options={{
+        search: false,
+        actionsColumnIndex: -1,
+        rowStyle: (x) => {
+          if (x.tableData.id % 2) {
+            return { backgroundColor: "#fafafa" };
+          }
+        },
+      }}
+      columns={columns}
+      data={data}
+      title={title}
+      editable={{
+        onRowUpdate: (rowData) =>
+          new Promise((resolve, reject) => {
+            setTimeout(() => {
+              updateById(rowData);
+              resolve();
+            }, 1000);
+          }),
+        onRowDelete: (rowData) =>
+          new Promise((resolve, reject) => {
+            setTimeout(() => {
+              if (rowData.id) deleteById(rowData.id);
+              resolve();
+            }, 1000);
+          }),
+      }}
+    />
   );
 }
 
--- client/src/pages/MainPage.jsx
+++ client/src/pages/MainPage.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import { useEffect } from "react";
 import { useSelector, useDispatch } from "react-redux";
 import Layout from "../components/Layout";
@@ -9,8 +9,12 @@ import {
   deleteById,
   updateById,
 } from "../actions/EmployeesHours.actions";
+import { DateService } from "../services/DateService";
+import TimeInput from "../components/TimeInput";
 
 function MainPage() {
+  const dateService = new DateService();
+  const [month] = useState(dateService.generateMonth());
   const dispatch = useDispatch();
   const employeeHours = useSelector((state) => state.employeeHours);
 
@@ -18,13 +22,63 @@ function MainPage() {
     dispatch(getAll());
   }, [dispatch]);
 
+  const editComponent = (props) => (
+    <TimeInput
+      type="text"
+      value={props.value}
+      onChange={(e) => props.onChange(e.target.value)}
+    />
+  );
+
+  const getTableData = () => {
+    return month.map((m, index) => {
+      employeeHours.data.forEach((d) => {
+        const isEqual = m.day === d.day;
+        if (isEqual) {
+          m = d;
+        }
+      });
+      return m;
+    });
+  };
+
   return (
     <Layout>
       <DatePanel />
       <Table
-        data={employeeHours.data || []}
+        data={getTableData()}
         deleteById={(id) => dispatch(deleteById(id))}
         updateById={(data) => dispatch(updateById(data))}
+        columns={[
+          {
+            title: "Day",
+            field: "day",
+            render: (rowData) =>
+              dateService.formatDate(rowData.day, "MM/DD/YYYY"),
+            editable: "never",
+          },
+          {
+            title: "Begin",
+            field: "begin",
+            editComponent: editComponent,
+          },
+          {
+            title: "Lunch Begin",
+            field: "lunchBegin",
+            editComponent: editComponent,
+          },
+          {
+            title: "Lunch End",
+            field: "lunchEnd",
+            editComponent: editComponent,
+          },
+          {
+            title: "End",
+            field: "end",
+            editComponent: editComponent,
+          },
+        ]}
+        title="Month and Year"
       />
     </Layout>
   );
